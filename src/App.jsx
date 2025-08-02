ad718ea0237111b153cc62e4f225559f041f79a1
chamodya <chamodyarajapaksha1@gmail.com>
2025-08-03 00:58:29 +0530

    develop basic ui structure for AI chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import styles from "./App.module.css";
 function App() {
   return (
-    <div>
-      <header>
-        <img src="/chat-bot.png" />
-        <h2>Ai Chat-Bot</h2>
+    <div className={styles.App}>
+      <header className={styles.header}>
+        <img className={styles.Logo} src="/chat-bot.png" />
+        <h2 className={styles.Title}>Ai Chat-Bot</h2>
       </header>
-      <div />
+      <div className={styles.ChatContainer} />
     </div>
   );
 }
