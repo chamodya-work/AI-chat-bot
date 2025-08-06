75dcb52e8497c8599faa2e9092f90add5a5459f3
chamodya <chamodyarajapaksha1@gmail.com>
2025-08-07 01:25:30 +0530

    difine key props and data-role

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,8 +1,10 @@
 export function Chat({ messages }) {
   return (
     <div>
-      {messages.map(({ role, content }) => (
-        <div>{content}</div>
+      {messages.map(({ role, content }, index) => (
+        <div key={index} data-role={role}>
+          {content}
+        </div>
       ))}
     </div>
   );
