b00a013c4913fc2438f2b54f32422e36887f5ddc
chamodya <chamodyarajapaksha1@gmail.com>
2025-08-07 01:32:35 +0530

    add chat.module,css file

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,8 +1,10 @@
+import styles from "./Chat.module.css";
+
 export function Chat({ messages }) {
   return (
-    <div>
+    <div className={styles.Chat}>
       {messages.map(({ role, content }, index) => (
-        <div key={index} data-role={role}>
+        <div className={styles.Message} key={index} data-role={role}>
           {content}
         </div>
       ))}
